fd05f877994248434f308c62cadd134c81554ccc
Chaunggwa <nautical19thureinsoe@gmail.com>
2023-10-23 00:52:07 +0630

    Replace unescaped entities

--- components/about.tsx
+++ components/about.tsx
@@ -5,9 +5,9 @@ const About = () => {
     <div className='flex flex-col mx-auto justify-center mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 p-6'>
         <h1 className='text-3xl font-medium capitalize'>About me</h1>
 
-        <p className='mb-3'>After successfully completing my Diploma in Nautical Science from Myanmar Mercantile Marine College, I decided to pursue my passion for programming. I studied full-stack web development from online courses on platforms like Udemy and Coursera. My favorite part of programming is the problem-solving aspect. I love the feeling of finally figuring out a solution to a problem. My core stack includes React, Next.js, Node.js, MySQL, and MongoDB and other utility libraries. Although I learned PHP and Laravel, I haven't dedicated as much time to them. I'm currently looking for a full-time position as a software developer.</p>
+        <p className='mb-3'>After successfully completing my Diploma in Nautical Science from Myanmar Mercantile Marine College, I decided to pursue my passion for programming. I studied full-stack web development from online courses on platforms like Udemy and Coursera. My favorite part of programming is the problem-solving aspect. I love the feeling of finally figuring out a solution to a problem. My core stack includes React, Next.js, Node.js, MySQL, and MongoDB and other utility libraries. Although I learned PHP and Laravel, I haven&apos;t dedicated as much time to them. I'm currently looking for a full-time position as a software developer.</p>
         <p>
-        When I'm not coding, I enjoy playing the guitar, listening to music, walking, reading, and playing football. I also like learning about philosophy, psychology, and mathematics.
+        When I&apos;m not coding, I enjoy playing the guitar, listening to music, walking, reading, and playing football. I also like learning about philosophy, psychology, and mathematics.
         </p>
     </div>
   )
